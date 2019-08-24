50333eacc050192d9e4706a1f312e37667a02053
cevatbarisyilmaz <cevatbarisyilmaz@gmail.com>
2019-08-24 03:14:38 +0300

    Update documentation

--- lossy.go
+++ lossy.go
@@ -5,20 +5,20 @@ Package lossy simulates bandwidth, latency and packet loss for net.PacketConn an
 */
 package lossy
 
-//Minimum Header Overhead for IPv4 based connections
+//IPv4MinHeaderOverhead is the minimum header overhead for IPv4 based connections.
 const IPv4MinHeaderOverhead = 20
 
-//Maximum Header Overhead for IPv4 based connections
+//IPv4MaxHeaderOverhead is the maximum header overhead for IPv4 based connections.
 const IPv4MaxHeaderOverhead = 60
 
-//Header Overhead for IPv6 based connections
+//IPv6HeaderOverhead is the header overhead for IPv6 based connections.
 const IPv6HeaderOverhead = 40
 
-//Minimum Header Overhead for UDP based connections over IPv4
+//UDPv4MinHeaderOverhead is the minimum header overhead for UDP based connections over IPv4.
 const UDPv4MinHeaderOverhead = 28
 
-//Maximum Header Overhead for UDP based connections over IPv4
+//UDPv4MaxHeaderOverhead is the maximum header overhead for UDP based connections over IPv4.
 const UDPv4MaxHeaderOverhead = 68
 
-//Header Overhead for UDP based connections over Ipv6
+//UDPv6HeaderOverhead is the header overhead for UDP based connections over Ipv6.
 const UDPv6HeaderOverhead = 48
