04e7a441d98f51dc0b16b51ab64205e4a999cf93
cevatbarisyilmaz <cevatbarisyilmaz@gmail.com>
2019-08-24 03:03:08 +0300

    Update constant values and package explanation

--- lossy.go
+++ lossy.go
@@ -2,16 +2,12 @@
 
 Package lossy simulates bandwidth, latency and packet loss for net.PacketConn and net.Conn interfaces.
 
-It can be used to test robustness of applications and network protocols runs over unreliable protocols such as UDP or IP.
-
 */
 package lossy
-
-const plainUDPHeaderOverHead = 8
 
 const IPv4MinHeaderOverhead = 20
 const IPv4MaxHeaderOverhead = 60
 const IPv6HeaderOverhead = 40
-const UDPv4MinHeaderOverhead = IPv4MinHeaderOverhead + plainUDPHeaderOverHead
-const UDPv4MaxHeaderOverhead = IPv4MaxHeaderOverhead + plainUDPHeaderOverHead
-const UDPv6HeaderOverhead = IPv6HeaderOverhead + plainUDPHeaderOverHead
+const UDPv4MinHeaderOverhead = 28
+const UDPv4MaxHeaderOverhead = 68
+const UDPv6HeaderOverhead = 48
