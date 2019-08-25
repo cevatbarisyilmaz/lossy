bf9db5a35016bd0e32d572de6ce3e0633dc83ff8
cevatbarisyilmaz <cevatbarisyilmaz@gmail.com>
2019-08-25 16:20:34 +0300

    Update documentation

--- lossy.go
+++ lossy.go
@@ -1,6 +1,6 @@
 /*
 
-Package lossy simulates bandwidth, latency and packet loss for net.NewPacketConn and net.NewConn interfaces.
+Package lossy simulates bandwidth, latency and packet loss for net.PacketConn and net.Conn interfaces.
 
 Its main usage is to test robustness of applications and network protocols run over unreliable transport protocols such as UDP or IP.
 As a side benefit, it can also be used as outbound bandwidth limiter.
